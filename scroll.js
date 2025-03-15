ca1cf823c54d4293c2ec1620f368bfe4673306e6
Luiz Vitor <luizpierinovitor@gmail.com>
2025-03-15 14:57:20 -0300

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -6,11 +6,11 @@ window.voltarVisibilidade = ScrollReveal({reset:true})
 
 voltarVisibilidade.reveal('.efeito-scroll-texto-topo',{
     duration:2000,
-    distance:'90px'
+    distance:'50px'
 })
 voltarVisibilidade.reveal('.efeito-scroll-texto-topo2',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     delay: 400
 })
 voltarVisibilidade.reveal('.efeito-scroll-texto-topo3',{
@@ -36,35 +36,35 @@ voltarVisibilidade.reveal('.efeito-scroll-img-topo',{
 
 voltarVisibilidade.reveal('.especialidades-scroll1',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
 })
 voltarVisibilidade.reveal('.especialidades-scroll2',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     delay: 300
 })
 
 
 voltarVisibilidade.reveal('.especialidades-scroll11',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     delay:600
 })
 voltarVisibilidade.reveal('.especialidades-scroll22',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     delay: 1000
 })
 
 
 voltarVisibilidade.reveal('.especialidades-scroll111',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     delay:1100
 })
 voltarVisibilidade.reveal('.especialidades-scroll222',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     delay: 1500,
     
 })
@@ -81,7 +81,7 @@ voltarVisibilidade.reveal('.scroll-img-sobre',{
 })
 voltarVisibilidade.reveal('.scroll-titulo',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     delay: 300,
     origin:'right'
 })
@@ -97,7 +97,7 @@ voltarVisibilidade.reveal('.scroll-a2',{
 })
 voltarVisibilidade.reveal('.scroll-a3',{
     duration:2000,
-    delay: 900
+    delay: 500
     
 })
 
@@ -105,7 +105,7 @@ voltarVisibilidade.reveal('.scroll-a3',{
 
 voltarVisibilidade.reveal('.scroll-prog',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     origin: 'bottom',
     delay: 100
     
@@ -114,37 +114,37 @@ voltarVisibilidade.reveal('.scroll-prog',{
 
 voltarVisibilidade.reveal('.scroll-titulo-form',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     origin: 'top',
     delay: 100
 })
 
 voltarVisibilidade.reveal('.scroll-input1',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     origin: 'right',
 })
 voltarVisibilidade.reveal('.scroll-input2',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     origin: 'left',
     delay: 200
 })
 voltarVisibilidade.reveal('.scroll-input3',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     origin: 'right',
     delay: 400
 })
 voltarVisibilidade.reveal('.scroll-input4',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     origin: 'left',
     delay: 600
 })
 voltarVisibilidade.reveal('.scroll-input5',{
     duration:2000,
-    distance:'90px',
+    distance:'50px',
     origin: 'bottom',
     delay: 800
 })
