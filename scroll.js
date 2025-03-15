fda268aeb02a91a05dd0065529a3260de6ffa057
Luiz Vitor <luizpierinovitor@gmail.com>
2025-03-15 14:22:55 -0300

    Melhorias

---
+++ scroll.js
@@ -0,0 +1,157 @@
+
+//Voltanto a visibilidade
+window.voltarVisibilidade = ScrollReveal({reset:true})
+
+//------------------Transformando ideias em código, e código em soluções.------------------------
+
+voltarVisibilidade.reveal('.efeito-scroll-texto-topo',{
+    duration:2000,
+    distance:'90px'
+})
+voltarVisibilidade.reveal('.efeito-scroll-texto-topo2',{
+    duration:2000,
+    distance:'90px',
+    delay: 400
+})
+voltarVisibilidade.reveal('.efeito-scroll-texto-topo3',{
+    duration:2000,
+    distance:'200px',
+    delay: 800,
+    origin:'left'
+})
+
+//--------------------------------------------------------------
+
+voltarVisibilidade.reveal('.efeito-scroll-img-topo',{
+    duration:2000,
+    delay: 1300,
+    distance:'150px',
+    origin:'right'
+
+    
+})
+
+//------------------Minhas especialidades.------------------------
+
+
+voltarVisibilidade.reveal('.especialidades-scroll1',{
+    duration:2000,
+    distance:'90px',
+})
+voltarVisibilidade.reveal('.especialidades-scroll2',{
+    duration:2000,
+    distance:'90px',
+    delay: 300
+})
+
+
+voltarVisibilidade.reveal('.especialidades-scroll11',{
+    duration:2000,
+    distance:'90px',
+    delay:600
+})
+voltarVisibilidade.reveal('.especialidades-scroll22',{
+    duration:2000,
+    distance:'90px',
+    delay: 1000
+})
+
+
+voltarVisibilidade.reveal('.especialidades-scroll111',{
+    duration:2000,
+    distance:'90px',
+    delay:1500
+})
+voltarVisibilidade.reveal('.especialidades-scroll222',{
+    duration:2000,
+    distance:'90px',
+    delay: 2000,
+    
+})
+
+
+
+
+
+//--------------------MUITO PRAZER. LUIZ VITOR.-------------------------
+voltarVisibilidade.reveal('.scroll-img-sobre',{
+    duration:2000,
+    distance:'400px',
+    origin:'left'
+})
+voltarVisibilidade.reveal('.scroll-titulo',{
+    duration:2000,
+    distance:'400px',
+    delay: 300,
+    origin:'right'
+})
+voltarVisibilidade.reveal('.scroll-texto',{
+    delay:1000
+})
+voltarVisibilidade.reveal('.scroll-a1',{
+    duration:2000
+})
+voltarVisibilidade.reveal('.scroll-a2',{
+    duration:2000,
+    delay: 500
+})
+voltarVisibilidade.reveal('.scroll-a3',{
+    duration:2000,
+    delay: 900
+    
+})
+
+//---------------------MEUS PROJETOS---------------------------
+
+voltarVisibilidade.reveal('.scroll-prog',{
+    duration:2000,
+    distance:'100px',
+    origin: 'bottom',
+    delay: 100
+    
+})
+//---------------------Seu contato é importante. Vamos conversar?---------------------------
+
+voltarVisibilidade.reveal('.scroll-titulo-form',{
+    duration:2000,
+    distance:'90px',
+    origin: 'top',
+    delay: 100
+})
+
+voltarVisibilidade.reveal('.scroll-input1',{
+    duration:2000,
+    distance:'500px',
+    origin: 'right',
+})
+voltarVisibilidade.reveal('.scroll-input2',{
+    duration:2000,
+    distance:'500px',
+    origin: 'left',
+    delay: 200
+})
+voltarVisibilidade.reveal('.scroll-input3',{
+    duration:2000,
+    distance:'500px',
+    origin: 'right',
+    delay: 400
+})
+voltarVisibilidade.reveal('.scroll-input4',{
+    duration:2000,
+    distance:'500px',
+    origin: 'left',
+    delay: 600
+})
+voltarVisibilidade.reveal('.scroll-input5',{
+    duration:2000,
+    distance:'300px',
+    origin: 'bottom',
+    delay: 800
+})
+
+//------------ RODAPÉ ---------------------
+voltarVisibilidade.reveal('.scroll-email',{
+    duration:2000,
+    distance:'1000px',
+    origin: 'left',
+})
