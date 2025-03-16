a7db2344802bc27c124c903a17d0bae57f3e8d4a
Luiz Vitor <luizpierinovitor@gmail.com>
2025-03-16 10:19:33 -0300

    Melhorias

--- scroll.js
+++ scroll.js
@@ -16,7 +16,7 @@ voltarVisibilidade.reveal('.efeito-scroll-texto-topo2',{
 voltarVisibilidade.reveal('.efeito-scroll-texto-topo3',{
     duration:2000,
     distance:'50px',
-    delay: 800,
+    delay: 700,
     origin:'bottom'
 })
 
@@ -45,24 +45,24 @@ voltarVisibilidade.reveal('.especialidades-scroll2',{
 voltarVisibilidade.reveal('.especialidades-scroll11',{
     duration:2000,
     distance:'50px',
-    delay:600
+    delay:400
 })
 voltarVisibilidade.reveal('.especialidades-scroll22',{
     duration:2000,
     distance:'50px',
-    delay: 1000
+    delay: 600
 })
 
 
 voltarVisibilidade.reveal('.especialidades-scroll111',{
     duration:2000,
     distance:'50px',
-    delay:1100
+    delay:900
 })
 voltarVisibilidade.reveal('.especialidades-scroll222',{
     duration:2000,
     distance:'50px',
-    delay: 1500,
+    delay: 1000,
     
 })
 
@@ -73,28 +73,35 @@ voltarVisibilidade.reveal('.especialidades-scroll222',{
 //--------------------MUITO PRAZER. LUIZ VITOR.-------------------------
 voltarVisibilidade.reveal('.scroll-img-sobre',{
     duration:2000,
-    distance:'90px',
-    origin:'bottom'
+    distance:'100px',
+    origin:'bottom',
+    delay:1000
 })
 voltarVisibilidade.reveal('.scroll-titulo',{
     duration:2000,
-    distance:'50px',
+    distance:'90px',
     delay: 300,
-    origin:'right'
+    origin:'top'
 })
 voltarVisibilidade.reveal('.scroll-texto',{
     delay:1000
 })
 voltarVisibilidade.reveal('.scroll-a1',{
-    duration:2000
+    duration:2000,
+    distance:'90px',
+    origin:'bottom'
 })
 voltarVisibilidade.reveal('.scroll-a2',{
     duration:2000,
-    delay: 500
+    delay: 500,
+    distance:'90px',
+    origin:'bottom'
 })
 voltarVisibilidade.reveal('.scroll-a3',{
     duration:2000,
-    delay: 500
+    delay: 900,
+    distance:'90px',
+    origin:'bottom'
     
 })
 
@@ -119,31 +126,31 @@ voltarVisibilidade.reveal('.scroll-titulo-form',{
 voltarVisibilidade.reveal('.scroll-input1',{
     duration:2000,
     distance:'50px',
-    origin: 'right',
+    origin: 'bottom',
 })
 voltarVisibilidade.reveal('.scroll-input2',{
     duration:2000,
-    distance:'50px',
-    origin: 'left',
+    distance:'60px',
+    origin: 'bottom',
     delay: 200
 })
 voltarVisibilidade.reveal('.scroll-input3',{
     duration:2000,
-    distance:'50px',
-    origin: 'right',
+    distance:'70px',
+    origin: 'bottom',
     delay: 400
 })
 voltarVisibilidade.reveal('.scroll-input4',{
     duration:2000,
-    distance:'50px',
-    origin: 'left',
+    distance:'80px',
+    origin: 'bottom',
     delay: 600
 })
 voltarVisibilidade.reveal('.scroll-input5',{
     duration:2000,
-    distance:'50px',
+    distance:'90px',
     origin: 'bottom',
-    delay: 800
+    delay: 700
 })
 
 //------------ RODAPÉ ---------------------
