ad0b889c2dcb71725f545a25f22c03379553fddf
Luiz Vitor <luizpierinovitor@gmail.com>
2025-05-15 09:38:20 -0300

    up

--- scroll.js
+++ scroll.js
@@ -5,26 +5,26 @@ window.voltarVisibilidade = ScrollReveal({reset:false})
 //------------------Transformando ideias em código, e código em soluções.------------------------
 
 voltarVisibilidade.reveal('.efeito-scroll-texto-topo',{
-    duration:2000,
+    duration:1000,
     distance:'50px'
 })
 voltarVisibilidade.reveal('.efeito-scroll-texto-topo2',{
-    duration:2000,
+    duration:1000,
     distance:'50px',
-    delay: 400
+    delay: 300
 })
 voltarVisibilidade.reveal('.efeito-scroll-texto-topo3',{
-    duration:2000,
+    duration:1000,
     distance:'50px',
-    delay: 700,
+    delay: 500,
     origin:'bottom'
 })
 
 //--------------------------------------------------------------
 
 voltarVisibilidade.reveal('.efeito-scroll-img-topo',{
-    duration:2000,
-    delay: 1300,
+    duration:1000,
+    delay: 900,
     
 })
 
@@ -38,31 +38,31 @@ voltarVisibilidade.reveal('.especialidades-scroll1',{
 voltarVisibilidade.reveal('.especialidades-scroll2',{
     duration:1000,
     distance:'50px',
-    delay: 300
+    delay: 200
 })
 
 
 voltarVisibilidade.reveal('.especialidades-scroll11',{
     duration:1000,
     distance:'50px',
-    delay:400
+    delay:300
 })
 voltarVisibilidade.reveal('.especialidades-scroll22',{
     duration:1000,
     distance:'50px',
-    delay: 600
+    delay: 500
 })
 
 
 voltarVisibilidade.reveal('.especialidades-scroll111',{
     duration:1000,
     distance:'50px',
-    delay:900
+    delay:700
 })
 voltarVisibilidade.reveal('.especialidades-scroll222',{
     duration:1000,
     distance:'50px',
-    delay: 1000,
+    delay: 800,
     
 })
 
@@ -75,7 +75,7 @@ voltarVisibilidade.reveal('.scroll-img-sobre',{
     duration:1000,
     distance:'100px',
     origin:'bottom',
-    delay:1000
+    delay:700
 })
 voltarVisibilidade.reveal('.scroll-titulo',{
     duration:1000,
@@ -84,7 +84,7 @@ voltarVisibilidade.reveal('.scroll-titulo',{
     origin:'top'
 })
 voltarVisibilidade.reveal('.scroll-texto',{
-    delay:1000
+    delay:700
 })
 voltarVisibilidade.reveal('.scroll-a1',{
     duration:1000,
@@ -93,13 +93,13 @@ voltarVisibilidade.reveal('.scroll-a1',{
 })
 voltarVisibilidade.reveal('.scroll-a2',{
     duration:1000,
-    delay: 500,
+    delay: 400,
     distance:'90px',
     origin:'bottom'
 })
 voltarVisibilidade.reveal('.scroll-a3',{
     duration:1000,
-    delay: 900,
+    delay: 600,
     distance:'90px',
     origin:'bottom'
     
@@ -138,19 +138,19 @@ voltarVisibilidade.reveal('.scroll-input3',{
     duration:1000,
     distance:'70px',
     origin: 'bottom',
-    delay: 400
+    delay: 300
 })
 voltarVisibilidade.reveal('.scroll-input4',{
     duration:1000,
     distance:'80px',
     origin: 'bottom',
-    delay: 600
+    delay: 500
 })
 voltarVisibilidade.reveal('.scroll-input5',{
     duration:1000,
     distance:'90px',
     origin: 'bottom',
-    delay: 700
+    delay: 560
 })
 
 //------------ RODAPÉ ---------------------
